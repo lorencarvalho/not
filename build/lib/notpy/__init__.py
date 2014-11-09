bd44c2fa6fcb120062080600ca08a070f3c15504
Loren Carvalho <lcarvalho@linkedin.com>
2014-11-09 13:17:30 -0800

    initial commit

---
+++ build/lib/notpy/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+class Not(object):
+    def __init__(self):
+        pass
+
+    def init(self):
+        print 'not!'
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='not',
+      author='Loren Carvalho',
+      version='0.0.1',
+      packages=['notpy'],
+      scripts=['not']
+      )
