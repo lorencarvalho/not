9f4c00593359432b74f640b7ac6abc1faf51ec74
Loren Carvalho <lcarvalho@linkedin.com>
2014-11-24 17:34:45 -0800

    starting to write out append/new logic... it currently sucks

--- notpy/__init__.py
+++ notpy/__init__.py
@@ -11,22 +11,37 @@ import config
 # evernote
 from evernote.api.client import EvernoteClient
 import evernote.edam.type.ttypes as Types
+import evernote.edam.notestore.ttypes as NoteStore
 
 client = EvernoteClient(token=config.DEVTOKEN)
-
 
 class Notary(object):
     def __init__(self, authtoken):
         self.authtoken = authtoken
+        self.store = client.get_note_store()
+        self.notefilter = NoteStore.NoteFilter()
 
     def search(self, title):
+        try:
+            # evernote's api is ridiculous
+            self.notefilter.words = 'intitle:"{0}"'.format(title)
+            notes_metadata_result_spec = NoteStore.NotesMetadataResultSpec()
+            notes_metadata_list = self.store.findNotesMetadata(self.notefilter, 0, 1, notes_metadata_result_spec)
+            note_guid = notes_metadata_list.notes[0].guid
+            note = self.store.getNote(note_guid, True, False, False, False)
+            return note_guid
+        except:
+            pass
 
-
-    def save(self, body, title=str(date.today())):
-        self.store = client.get_note_store()
-        self.note = Types.Note()
+    def save(self, body, title):
+        if self.search(title):
+            self.note = self.store.getNote(note_guid, True, False, False, False)
+            save_it = self.store.updateNote
+        else:
+            self.note = Types.Note()
+            save_it = self.store.createNote
         self.note.title = title
         self.note.content = '<?xml version="1.0" encoding="UTF-8"?>'
         self.note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
         self.note.content += '<en-note>{0}</en-note>'.format(body)
-        self.store.createNote(self.note)
+        save_it(self.note)
