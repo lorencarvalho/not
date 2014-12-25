9a0fa328b8fa8b0f334e6eac13dd80a31a218162
Loren Carvalho <lcarvalho@linkedin.com>
2014-12-24 22:12:34 -0800

    silencng httprequesthandler

--- notpy/oauth.py
+++ notpy/oauth.py
@@ -8,17 +8,29 @@ from evernote.api.client import EvernoteClient
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        '''
+        serve the callback from evernote oauth
+        '''
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write("<html><head><title>Huzzah!</title></head>")
-        self.wfile.write("<body><p>Success!</p>")
+        self.wfile.write("<body><p>Successfully authorized with `not`</p>")
         self.wfile.close()
         global path
         path = self.path
 
+    def log_message(self, format, *args):
+        '''
+        shutup logs
+        '''
+        return
+
 
 def parse_query_string(authorize_url):
+    '''
+    unpack the query string
+    '''
     uargs = authorize_url.split('?')
     vals = {}
     for pair in uargs[1].split('&'):
@@ -28,6 +40,9 @@ def parse_query_string(authorize_url):
 
 
 def serve_one_request(server_class=HTTPServer, handler_class=Handler):
+    '''
+    returns the subclassed http server
+    '''
     server_address = ('', 10668)
     httpd = server_class(server_address, handler_class)
     return httpd
@@ -45,9 +60,8 @@ def setup():
                 sandbox = False
             )
     request_token = client.get_request_token('http://localhost:10668')
-    print "Paste this URL in your browser and login"
-    print client.get_authorize_url(request_token)
-    print "Paste the URL you get after logging in here:"
+    print "Paste this URL in your browser and login:"
+    print '  ', client.get_authorize_url(request_token)
     serve_one_request().handle_request()
     authurl = path
     vals = parse_query_string(authurl)
@@ -58,5 +72,4 @@ def setup():
                 )
     with open(os.path.join(os.environ['HOME'], '.not_token'), 'w') as f:
         f.write(auth_token)
-        client = EvernoteClient(token=auth_token, sandbox=False)
-        client.get_note_store()
+        print "Token saved."
