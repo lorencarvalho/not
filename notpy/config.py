9cd3206f7824d1fd0bb686ace0002b0874fd3aca
Loren Carvalho <lcarvalho@linkedin.com>
2015-01-04 17:13:42 -0800

    disclaimer

--- notpy/config.py
+++ notpy/config.py
@@ -11,4 +11,9 @@ def token():
 EDITOR = os.environ.get('EDITOR', 'vim')
 TOKEN = token()
 CONSUMER_KEY = 'comradeloren'
+
+# ok, so, evernote doesn't have any resources on whether or not this 
+# "secret" should remain a secret, and other evernote apps on github
+# have this in their revision control, best I can tell you have to have it
+# in order to instantiate the oauth client and whatnot. fuck it.
 CONSUMER_SECRET = 'c219754d19198564'
